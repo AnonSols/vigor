4117775e0c76694940eefb8871ef686a7542408c
Anon Sols <soloschmail@gmail.com>
2024-06-04 23:37:36 +0100

    fix minor bugs

--- src/data/Uploader.tsx
+++ src/data/Uploader.tsx
@@ -48,24 +48,24 @@ async function createBookings() {
     .from("guests")
     .select("id")
     .order("id");
-  const allGuestIds = guestsIds.map((cabin) => cabin.id);
+  const allGuestIds = guestsIds && guestsIds.map((cabin) => cabin.id);
 
   //cabinsIds
   const { data: cabinsIds } = await supabase
     .from("cabins")
     .select("id")
     .order("id");
-  const allCabinIds = cabinsIds.map((cabin) => cabin.id);
+  const allCabinIds = cabinsIds && cabinsIds.map((cabin) => cabin.id);
 
   const finalBookings = bookings.map((booking) => {
     // Here relying on the order of cabins, as they don't have and ID yet
     const cabin = cabins.at(booking.cabinId - 1);
     const numNights = subtractDates(booking.endDate, booking.startDate);
-    const cabinPrice = numNights * (cabin.regularPrice - cabin.discount);
+    const cabinPrice = cabin && numNights * cabin.regularPrice - cabin.discount;
     const extrasPrice = booking.hasBreakfast
       ? numNights * 15 * booking.numGuests
       : 0; // hardcoded breakfast price
-    const totalPrice = cabinPrice + extrasPrice;
+    const totalPrice = cabinPrice && cabinPrice + extrasPrice;
 
     let status;
     if (
@@ -92,8 +92,8 @@ async function createBookings() {
       cabinPrice,
       extrasPrice,
       totalPrice,
-      guestId: allGuestIds.at(booking.guestId - 1),
-      cabinId: allCabinIds.at(booking.cabinId - 1),
+      guestId: allGuestIds && allGuestIds.at(booking.guestId - 1),
+      cabinId: allCabinIds && allCabinIds.at(booking.cabinId - 1),
       status,
     };
   });

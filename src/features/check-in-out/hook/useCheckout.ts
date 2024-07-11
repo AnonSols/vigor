eafac7c1912c7575d8f0aeaf60189a5d3f96e681
Anon Sols <soloschmail@gmail.com>
2024-07-11 02:09:53 +0100

    minor changes

--- src/features/check-in-out/hook/useCheckout.ts
+++ src/features/check-in-out/hook/useCheckout.ts
@@ -1,13 +1,11 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query"
 import { updateBooking } from "../../../services/apiBookings"
-import toast from "react-hot-toast"
-import { useNavigate } from "react-router-dom"
+import toast from "react-hot-toast" 
 import { tableData } from "../../../../types";
 import { BookingType } from "../../../../types/bookingsTypes";
 
 export function useCheckout() {
 
-    const navigate = useNavigate();
 
     const queryClient = useQueryClient();
     const {mutate:checkOut, isLoading:isCheckingOut} = useMutation({
@@ -16,8 +14,7 @@ export function useCheckout() {
         onSuccess:(data:BookingType) =>{
              const {id}= data;
     toast.success(`Booking ${id} successfully Checked Out `)
-    queryClient.invalidateQueries({queryKey:[tableData.BOOKINGS]},)
-            navigate('/');
+    queryClient.invalidateQueries({queryKey:[tableData.BOOKINGS]},) 
         }, 
 
         onError:()=>  toast.error("There was an error checking out")

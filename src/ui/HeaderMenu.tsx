961420d87b75e7f85e4f08f934433fb3f8624b65
Anon Sols <soloschmail@gmail.com>
2024-07-28 00:17:06 +0100

    add tooltip

--- src/ui/HeaderMenu.tsx
+++ src/ui/HeaderMenu.tsx
@@ -4,24 +4,77 @@ import HeaderTab from "./HeaderTab";
 import { useNavigate } from "react-router-dom";
 import { HiOutlineUser } from "react-icons/hi2";
 
+const StyledHeader = styled.ul`
+  display: flex;
+  gap: 0.4rem;
+  align-items: center;
+  gap: 0.4rem;
+`;
+
+const Tooltip = styled.div`
+  /* Tooltip container */
+
+  position: relative;
+  display: inline-block;
+
+  /* Tooltip text */
+  .tooltiptext {
+    visibility: hidden;
+    // width: 120px;
+    background-color: #555;
+    color: #fff;
+    text-align: center;
+    padding: 5px;
+    border-radius: 6px;
+
+    /* Position the tooltip text */
+    position: absolute;
+    z-index: 1;
+    bottom: -100%;
+    left: 50%;
+    margin-left: -30px;
+
+    /* Fade in tooltip */
+    opacity: 0;
+    transition: opacity 0.3s;
+  }
+
+  /* Tooltip arrow */
+  .tooltiptext::after {
+    content: "";
+    position: absolute;
+    bottom: 100%;
+    left: 50%;
+    margin-left: -5px;
+    border-width: 5px;
+    border-style: solid;
+    border-color: #555 transparent transparent transparent;
+  }
+
+  /* Show the tooltip text when you mouse over the tooltip container */
+  &:hover .tooltiptext {
+    visibility: visible;
+    opacity: 1;
+  }
+`;
 const HeaderMenu = () => {
-  const StyledHeader = styled.ul`
-    display: flex;
-    gap: 0.4rem;
-    align-items: center;
-    gap: 0.4rem;
-  `;
   const navigate = useNavigate();
   return (
     <StyledHeader>
       <li>
-        <HeaderTab
-          onClick={() => navigate("/account")}
-          icon={<HiOutlineUser />}
-        />
+        <Tooltip>
+          <HeaderTab
+            onClick={() => navigate("/account")}
+            icon={<HiOutlineUser />}
+          />
+          <div className="tooltiptext">Account</div>
+        </Tooltip>
       </li>
       <li>
-        <Logout />
+        <Tooltip>
+          <Logout />
+          <div className="tooltiptext">Logout</div>
+        </Tooltip>
       </li>
     </StyledHeader>
   );
